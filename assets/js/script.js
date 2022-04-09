0f8386b4afe82eecbb49c9bb4d6e1a6f5c5e0383
John Hysong <johnahysong@gmail.com>
2022-04-09 13:20:09 -0400

    added day js script for times

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@ const weatherApiUrl = "https://api.openweather.org";
 const apiKey = "21e6cc9ea27c09ef65e174309fcb718d"
 
 // DOM elements
-let searchForm
-let searchInput
-let todayBox
-let forecastBox
-let searchDisplayBox
+let searchForm = document.querySelector("#search-form");
+let searchInput = document.querySelector("#search-form");
+let todayBox = document.querySelector("#todays-weather");
+let forecastBox = document.querySelector("#five-day-forecast");
+let searchDisplayBox = document.querySelector("#search-display");
 
 //add timezine for day.js
 dayjs.extend(window.dayjs_plugin_utc);
