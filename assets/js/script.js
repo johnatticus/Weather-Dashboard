5a2fe427cd7635eaa44e98e02e33aed89798ebef
John Hysong <johnahysong@gmail.com>
2022-04-10 23:28:36 -0400

    added to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 const searchHistory = [];
-const weatherApiUrl = "https://api.openweather.org";
+// const weatherApiUrl = "https://api.openweather.org";
 const apiKey = "21e6cc9ea27c09ef65e174309fcb718d"
 
 // DOM elements
 let searchForm = document.querySelector("#search-form");
 let searchInput = document.querySelector("#search-form");
-let todayBox = document.querySelector("#todays-weather");
+let todayBox = document.querySelector("#weather-result");
 let forecastBox = document.querySelector("#five-day-forecast");
 let searchDisplayBox = document.querySelector("#search-display");
 
@@ -18,3 +18,18 @@ function displaySearchHistory() {
 
 }
 
+var weatherApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`;
+
+function weatherData() {
+    fetch(weatherApiUrl)
+    .then(function(res) {
+      return res.json()        
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+}
+
+weatherData();
+
+// http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={apiKey}
