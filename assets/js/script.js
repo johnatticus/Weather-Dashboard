774902325f89ea7771b481f59c927d7c58934c6d
John Hysong <johnahysong@gmail.com>
2022-04-11 01:36:44 -0400

    added initial js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,35 @@
+var citySearchText = document.querySelector("#city-search-text");
+var citySearchForm = document.querySelector("#search-form");
+
 const searchHistory = [];
+
 // const weatherApiUrl = "https://api.openweather.org";
 const apiKey = "21e6cc9ea27c09ef65e174309fcb718d"
-
-// DOM elements
-let searchForm = document.querySelector("#search-form");
-let searchInput = document.querySelector("#search-form");
-let todayBox = document.querySelector("#weather-result");
-let forecastBox = document.querySelector("#five-day-forecast");
-let searchDisplayBox = document.querySelector("#search-display");
 
 //add timezine for day.js
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
+var searchCityEntered = function(event){
+    event.preventDefault();
+
+    var cityName = citySearchText.value.trim();
+    
+    if(cityName){
+        getCityWeather(cityName);
+        getCityFiveDayForecast(cityName);
+    }else{
+        alert("Please enter a city name");
+    }
+    
+}
+
 //function to display search history
-function displaySearchHistory() {
+// function displaySearchHistory() {
 
-}
+// }
 
-var weatherApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`;
-
-function weatherData() {
-    fetch(weatherApiUrl)
-    .then(function(res) {
-      return res.json()        
-    })
-    .then(function(data) {
-        console.log(data);
-    })
-}
-
-weatherData();
 
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={apiKey}
+
+citySearchForm.addEventListener("click", searchCityEntered);
