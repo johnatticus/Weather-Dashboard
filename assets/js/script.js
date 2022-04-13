a6cb8511e56630a96d0ee016151e5d83476c42c2
John Hysong <johnahysong@gmail.com>
2022-04-12 20:39:06 -0400

    fixed js code and added UV index with colors. Broke city name.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,19 @@ const searchHistory = [];
 // const weatherApiUrl = "https://api.openweather.org";
 const apiKey = "21e6cc9ea27c09ef65e174309fcb718d"
 
+// var weatherApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Orlando&limit=5&appid=21e6cc9ea27c09ef65e174309fcb718d";
+
+// function weatherData() {
+//     fetch(weatherApiUrl)
+//     .then(function(res) {
+//       return res.json()        
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//     })
+// }
+
+// weatherData();
 //add timezine for day.js
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
@@ -21,44 +34,76 @@ var searchCityEntered = function(event){
 var cityName = citySearchText.value.trim();
     
 if (cityName) {
-    getWeather(cityName);
-    getFiveDay(cityName);
+    getLatLon(cityName);
+    // getFiveDay(cityName);
 } else {
     alert("Please enter a city name");
     }
     // console.log(cityName)
 }
 
-function getWeather(cityName) {
-    var geoAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=21e6cc9ea27c09ef65e174309fcb718d";
-
-    fetch(geoAPI).then(function(response){
-        if(response.ok){
-            response.json().then(function(APIdata){
-                displayCurrentTemp(APIdata);
-                console.log(response)
-            });
-        }else{
-            alert("Error: " + response.statusText);
-        }
+function getLatLon(cityName) {
+    console.log(cityName)
+    if(cityName) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?appid=21e6cc9ea27c09ef65e174309fcb718d&q=${cityName}&=&=`)
+    .then(function(res) {
+      return res.json()
     })
-    .catch(function(error){
-        alert("unable to connect");
+    .then(function(data) {
+        console.log(data);
+        weatherData(data)
     })
+    }   
+}
+  
+  
+  function weatherData(data) {
+    var latitude = data[0].lat
+    var longitude = data[0].lon
+    console.log(latitude)
+    console.log(longitude);
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?appid=21e6cc9ea27c09ef65e174309fcb718d&lat=${latitude}&lon=${longitude}&units=imperial`)
+      .then(function(res) {
+        return res.json()
+      })
+      .then(function(data) {
+          console.log(data);
+          displayCurrentTemp(data);
+          
+      })
 }
 
-var displayCurrentTemp = function(APIdata){
-    
-    var city = APIdata.name;
-    var icon = APIdata.weather[0].icon;
-    var temp = APIdata.main.temp;
-    var humidity = APIdata.main.humidity;
-    var windSpeed = APIdata.wind.speed;
-    var lat = APIdata.coord.lat;
-    var lon = APIdata.coord.lon;
-    // var UV = data.current.uvi;
+// function getWeather(cityName) {
+//     var geoAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=21e6cc9ea27c09ef65e174309fcb718d";
+
+//     fetch(geoAPI).then(function(response){
+//         if(response.ok){
+//             response.json().then(function(APIdata){
+//                 displayCurrentTemp(APIdata);
+//                 console.log(response)
+//             });
+//         }else{
+//             alert("Error: " + response.statusText);
+//         }
+//     })
+//     .catch(function(error){
+//         alert("unable to connect");
+//     })
+// }
+
+var displayCurrentTemp = function(data){
+    console.log(data.daily)
+    var city = data.name;
+    var icon = data.current.weather[0].icon;
+    var temp = data.current.temp;
+    var humidity = data.current.humidity;
+    var windSpeed = data.current.wind_speed;
+    var uvi = data.current.uvi;
+    // var lat = data.coord.lat;
+    // var lon = data.coord.lon;
 
     console.log(temp)
+
 
     forecastContainer.textContent = "";
 
@@ -95,9 +140,39 @@ var displayCurrentTemp = function(APIdata){
 
     todayWeatherCard.appendChild(todayWind);
 
-    // var todayUV = document.createElement("div");
-    // todayUV.textContent = "UV rating: " + data.current.uvi;
+    var todayUV = document.createElement("div");
+    
+    if (uvi <= 2) {
+        todayUV.className = "card-body low"
+        todayUV.textContent = "UV rating: " + uvi;
+        todayWeatherCard.appendChild(todayUV);
+    }
 
+    if (uvi > 2 && uvi <= 5) {
+        todayUV.className = "card-body moderate"
+        todayUV.textContent = "UV rating: " + uvi;
+        todayWeatherCard.appendChild(todayUV);
+    }
+    
+    if (uvi > 5 && uvi <= 7) {
+        todayUV.className = "card-body high"
+        todayUV.textContent = "UV rating: " + uvi;
+        todayWeatherCard.appendChild(todayUV);
+    }
+
+    if (uvi > 7 && uvi <= 10) {
+        todayUV.className = "card-body veryhigh"
+        todayUV.textContent = "UV rating: " + uvi;
+        todayWeatherCard.appendChild(todayUV);
+    }
+
+    if (uvi > 10) {
+        todayUV.className = "card-body extreme"
+        todayUV.textContent = "UV rating: " + uvi;
+        todayWeatherCard.appendChild(todayUV);
+    }
+    // todayUV.textContent = "UV rating: " + uvi;
+    
     // todayWeatherCard.appendChild(todayUV);
 };
 
