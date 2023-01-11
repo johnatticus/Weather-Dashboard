298bc25646ab70aaef0fb4ec83bf6f1b7b99c1c6
John H <johnatticus@Johns-MacBook-Pro.local>
2023-01-10 21:38:49 -0500

    storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,58 @@
-searchStorage = [];
+var searchStorage = [];
 
 // variables to traverse DOM
 var citySearchText = document.querySelector("#city-search-text");
-var citySearchButton = document.querySelector("#search-button");
+var citySearchForm = document.querySelector("#search-form");
 var bigForecast = document.querySelector("#today-forecast");
 var fiveDayArea = document.querySelector("#five-day-deck");
 var forecastContainer = document.querySelector("#forecast-area");
+var searchHistoryContainer = document.querySelector('#history');
 
 var apiKey = "21e6cc9ea27c09ef65e174309fcb718d";
 
 // add timezine for day.js
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
+
+// function to display search history
+// work in progress
+function displaySearchHistory() {
+    searchHistoryContainer.innerHTML = "";
+  
+    // Start at end of history array and count down to show the most recent at the top.
+    for (var i = searchStorage.length - 1; i >= 0; i--) {
+      var btn = document.createElement("button");
+      btn.setAttribute("type", "button");
+      btn.setAttribute("aria-controls", "today forecast");
+      btn.classList.add("btn", "btn-block", "btn-secondary", "mt-2", "mb-2");
+  
+      // `data-search` allows access to city name when click handler is invoked
+      btn.setAttribute("data-search", searchStorage[i]);
+      btn.textContent = searchStorage[i];
+      searchHistoryContainer.append(btn);
+    }
+  }
+
+  // Function to update history in local storage then updates displayed history.
+function appendToHistory(cityName) {
+    // If there is no search term return the function
+    if (searchStorage.indexOf(cityName) !== -1) {
+      return;
+    }
+    searchStorage.push(cityName);
+  
+    localStorage.setItem("city-search-history", JSON.stringify(searchStorage));
+    displaySearchHistory();
+  }
+  
+  // Function to get search history from local storage
+  function initSearchHistory() {
+    var storedHistory = localStorage.getItem("city-search-history");
+    if (storedHistory) {
+      searchStorage = JSON.parse(storedHistory);
+    }
+    displaySearchHistory();
+  }
 
 // get the text input from user and turn into a var
 // if nothing entered, present an alert
@@ -23,6 +64,7 @@ var searchCityEntered = function (event) {
 
   if (cityName) {
     getLatLon(cityName);
+    citySearchText.value = '';
     // getFiveDay(cityName);
   } else {
     alert("Please enter a city name");
@@ -44,6 +86,7 @@ function getLatLon(cityName) {
       .then(function (data) {
         console.log(data);
         weatherData(data);
+        appendToHistory(cityName);
       });
   } else {
     alert("Error: " + response.statusTest);
@@ -77,17 +120,15 @@ function weatherData(data) {
 var displayCurrentTemp = function (city, data) {
   timezone = data.timezone;
   var date = dayjs().tz(timezone).format("M/D/YYYY");
-  console.log(data);
-  console.log(data.daily);
+  //   console.log(data);
+  //   console.log(data.daily);
   var icon = data.current.weather[0].icon;
   var temp = data.current.temp;
   var humidity = data.current.humidity;
   var windSpeed = data.current.wind_speed;
   var uvi = data.current.uvi;
-
-  console.log(temp);
-
-  forecastContainer.textContent = "";
+  //   console.log(temp);
+  forecastContainer.innerHTML = "";
 
   var todayWeatherCard = document.createElement("div");
   todayWeatherCard.className = "card";
@@ -95,64 +136,68 @@ var displayCurrentTemp = function (city, data) {
   var cityNameText = document.createElement("h4");
   cityNameText.className = "card-header bg-primary text-white";
   cityNameText.textContent = `${city} ${date}`;
-  console.log(city);
 
-  forecastContainer.appendChild(todayWeatherCard);
-  todayWeatherCard.appendChild(cityNameText);
+  var todayWeatherCardBody = document.createElement("div");
+  todayWeatherCardBody.className = "card-body";
 
   var weatherIcon = document.createElement("img");
   weatherIcon.src = "http://openweathermap.org/img/wn/" + icon + ".png";
-  todayWeatherCard.appendChild(weatherIcon);
+  //   todayWeatherCard.appendChild(weatherIcon);
 
-  var todayTemp = document.createElement("div");
-  todayTemp.className = "card-body";
+  var todayTemp = document.createElement("p");
+  todayTemp.className = "card-text";
   todayTemp.textContent = "Temperature: " + temp + "°F";
 
-  todayWeatherCard.appendChild(todayTemp);
+  //   todayWeatherCard.appendChild(todayTemp);
 
-  var todayHumidity = document.createElement("div");
-  todayHumidity.className = "card-body";
+  var todayHumidity = document.createElement("p");
+  todayHumidity.className = "card-text";
   todayHumidity.textContent = "Humidity: " + humidity + "%";
 
-  todayWeatherCard.appendChild(todayHumidity);
+  //   todayWeatherCard.appendChild(todayHumidity);
 
-  var todayWind = document.createElement("div");
-  todayWind.className = "card-body";
+  var todayWind = document.createElement("p");
+  todayWind.className = "card-text";
   todayWind.textContent = "Wind Speed: " + windSpeed + " m/s";
 
-  todayWeatherCard.appendChild(todayWind);
+  //   todayWeatherCard.appendChild(todayWind);
 
-  var todayUV = document.createElement("div");
+  var todayUV = document.createElement("p");
+
+  forecastContainer.appendChild(todayWeatherCard);
+  todayWeatherCard.appendChild(cityNameText);
+  todayWeatherCard.appendChild(todayWeatherCardBody);
+  todayWeatherCardBody.append(weatherIcon, todayTemp, todayHumidity, todayWind);
 
   // if statements to change the text color of UV based on UV index colors
   if (uvi <= 2) {
-    todayUV.className = "card-body low";
+    todayUV.className = "card-text low";
     todayUV.textContent = "UV rating: " + uvi;
-    todayWeatherCard.appendChild(todayUV);
+    todayWeatherCardBody.appendChild(todayUV);
   }
 
   if (uvi > 2 && uvi <= 5) {
-    todayUV.className = "card-body moderate";
+    todayUV.className = "card-text moderate";
     todayUV.textContent = "UV rating: " + uvi;
-    todayWeatherCard.appendChild(todayUV);
+    todayWeatherCardBody.appendChild(todayUV);
   }
 
   if (uvi > 5 && uvi <= 7) {
-    todayUV.className = "card-body high";
+    todayUV.className = "card-text high";
     todayUV.textContent = "UV rating: " + uvi;
-    todayWeatherCard.appendChild(todayUV);
+    todayWeatherCardBody.appendChild(todayUV);
   }
 
   if (uvi > 7 && uvi <= 10) {
-    todayUV.className = "card-body veryhigh";
+    todayUV.className = "card-text veryhigh";
     todayUV.textContent = "UV rating: " + uvi;
-    todayWeatherCard.appendChild(todayUV);
+    todayWeatherCardBody.appendChild(todayUV);
   }
 
   if (uvi > 10) {
-    todayUV.className = "card-body extreme";
+    todayUV.className = "card-text extreme";
     todayUV.textContent = "UV rating: " + uvi;
-    todayWeatherCard.appendChild(todayUV);
+    todayWeatherCardBody.appendChild(todayUV);
   }
 };
 
@@ -187,31 +232,47 @@ var getFiveDay = function (data) {
     var weatherIcon = document.createElement("img");
     weatherIcon.src = "http://openweathermap.org/img/wn/" + icon + ".png";
 
-    var todayTemp = document.createElement("p");
-    todayTemp.className = "card-text";
-    todayTemp.textContent = "Temperature: " + temp + "°F";
+    var fiveDayTemp = document.createElement("p");
+    fiveDayTemp.className = "card-text";
+    fiveDayTemp.textContent = "Temperature: " + temp + "°F";
 
-    var todayHumidity = document.createElement("p");
-    todayHumidity.className = "card-text";
-    todayHumidity.textContent = "Humidity: " + humidity + "%";
+    var fiveDayHumidity = document.createElement("p");
+    fiveDayHumidity.className = "card-text";
+    fiveDayHumidity.textContent = "Humidity: " + humidity + "%";
 
-    var todayWind = document.createElement("p");
-    todayWind.className = "card-text";
-    todayWind.textContent = "Wind Speed: " + windSpeed + " m/s";
+    var fiveDayWind = document.createElement("p");
+    fiveDayWind.className = "card-text";
+    fiveDayWind.textContent = "Wind Speed: " + windSpeed + " m/s";
 
     fiveDayArea.appendChild(fiveDayCard);
     fiveDayCard.appendChild(fiveDayDate);
     fiveDayCard.appendChild(fiveDayCardBody);
-    fiveDayCardBody.append(weatherIcon, todayTemp, todayHumidity, todayWind);
+    fiveDayCardBody.append(
+      weatherIcon,
+      fiveDayTemp,
+      fiveDayHumidity,
+      fiveDayWind
+    );
 
     if (i > 4) {
       break;
     }
   }
 };
-// function to display search history
-// work in progress
-function displaySearchHistory() {}
+
+function searchStorageClick(e) {
+    // Don't do search if current elements is not a search history button
+    if (!e.target.matches('.btn-secondary')) {
+      return;
+    }
+  
+    var btn = e.target;
+    var search = btn.getAttribute('data-search');
+    getLatLon(search);
+  }
+
+initSearchHistory();
 
 // listen for click on search button
-citySearchButton.addEventListener("click", searchCityEntered);
+citySearchForm.addEventListener("submit", searchCityEntered);
+searchHistoryContainer.addEventListener("click", searchStorageClick)
